7a76505a587425b615a7a7ce4654c527eb422896
EstebanRS1 <esteban.roberto@udea.edu.co>
2023-02-21 09:03:16 -0500

    Version1

---
+++ Clinica_Veterinaria.py
@@ -0,0 +1,37 @@
+class Mascota:
+    def __init__(self):
+        self.__nombre="hola"
+        self.__historia=""
+        self.__peso=0
+        self.__medicamento=""
+        self.__fecha_ingreso=""
+    
+    #Getters
+    def verNombre(self):
+        return self.__nombre
+    
+    def verHistoria(self):
+        return self.__historia
+    
+    def verPeso(self):
+        return self.__peso
+
+    def verMedicamento(self):
+        return self.__medicamento
+    
+    def verFecha(self):
+        return self.__fecha_ingreso
+    
+    #Setters
+    def asignarNombre(self,N):
+        self.__nombre=N
+
+    def asignarHistoria(self,H):
+        self.__historia=H
+
+    def asignarMedicamento(self,M):
+        self.__medicamento=M
+
+    def asignarFecha(self,F):
+        self.__fecha_ingreso=F
+
