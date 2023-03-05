0dcc665724318bbdbb3993f2910d02f3bd2bfba2
EstebanRS1 <esteban.roberto@udea.edu.co>
2023-03-05 18:40:59 -0500

    Version Final

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -48,9 +48,10 @@ class Visita:
     def __init_ (self):
     #Atributos de la clase
         self.__fecha = datetime.datetime.now().strftime("%x")
-        self. __registro = os.getcwdb()
+        self. __registro = "" #os.getcwdb()
         self. __notas = ""
-        self. __indice = Indices()
+        self. __indice = Indices() ## Solo se guarda un objeto tipo Indices
+    
     #Metodos de la clase        
     def verfecha(self):
         return self.__fecha
@@ -72,73 +73,229 @@ class Visita:
 
 class Paciente:
     def __init__(self):
-        self.nombre = ""
-        self.cedula = 0
-        self.genero=""
-        self.visita=str
-        self.dict_Visitas={}
+        self.__nombre = ""
+        self.__cedula = 0
+        self.__genero=""
+        #self.visita=str
+        #Un paciente tiene muchas visitas, en este ejemplo los trabajamos
+        #como diccionarios
+        self.__visitas={} # Clave-->fecha, valor-->Visita
+
     def verNombre(self):
-        return self.nombre
+        return self.__nombre
+    def asignarNombre(self,n):
+        self.__nombre=n
     def verCedula(self):
-        return self.cedula
+        return self.__cedula
+    def asignarCedula(self,c):
+        self.__cedula=c
     def verGenero(self):
-        return self.genero
-    # def verVisita(self):
-    #     return self.visita
+        return self.__genero
+    def asignarGenero(self,g):
+        self.__genero=g
+
+    def verVisita(self,f):
+        return self.visita.get(f)
     # def verDict_Visitas(self):
     #     return self.dict_Visitas
-    
-    def asignarNombre(self,n):
-        self.nombre=n
-    def asignarCedula(self,c):
-        self.cedula=c
-    def asignarGenero(self,g):
-        self.genero=g
     def asignarVisita(self,v):
-        self.visita=v
-    def asignarDict_Visitas(self,d):
-        self.dict_Visitas=d
+        self.__visitas[v.verFecha]=v
+
+    def verListadoVisitas(self):
+        return self.__visitas
     
+    def verificarExiste(self,f):
+        return f in self.__visitas
+    
+    def eliminarVisita(self,f):
+        del self.__visitas[f]
 class Sistema:
     def __init__(self):
-        self.__Pacientes={}
+        self.__pacientes={}#Clave --> cedula y Valor --> el paciente
     
     def verificarExiste(self,c):
-        if c in self.__Pacientes:
-            return True
-        else:
-            return False
+        return c in self.__pacientes
+        #     return True
+        # else:
+        #     return False
     def agregarPaciente(self,p):
-        self.__Pacientes[p.verCedula()]=p
+        self.__pacientes[p.verCedula()]=p
 
     def eliminarPac(self,c):
-        del self.__Pacientes[c]
+        del self.__pacientes[c]
         return True
     
-    def recuperarPac(self,c):
-        return self.__Pacientes[c]
+    def verPac(self,c):
+        return self.__pacientes[c]
     
     def cargarGuardar(self):
         pass
-    def validar(self,c):
-        while True:
-            try:
-                valor=int(input(c))
-                return valor
-            except:
-                print("Error, ingrese un valor valido")
-        
+
+def validar(msj):
+    while True:
+        try:
+            valor = int(input(msj))
+            break
+        except ValueError:
+            print("Error!, Ingrese un dato numérico...")
+    return valor
+
+def validarf(cc):
+    while True:
+        try:
+            valor=float(input(cc))
+            break
+        except ValueError:
+            print("Error, ingrese un valor valido")
+    return valor
+    
+
 def main():
     sis =Sistema()
     while True:
-        print("Ingrese: \n1. Nuevo Paciente \n2. Editar Paciente \n3. Eliminar Paciente \n4. Cargar/Guardar \n5. Salir")
-        valor=sis.validar("Valor: ")
+        print("Ingrese: \n1. Nuevo Paciente \n2. Editar Paciente \n3. Eliminar Paciente \n4. Cargar/Guardar \n5. Ver cantidad de visitas de pacientes \n6.Salir")
+        valor=validar("Valor: ")
 
         if valor == 1:
-            cedula= sis.validar("Ingrese la Cedula: ")
+            cedula= validar("Ingrese la Cedula: ")
             if sis.verificarExiste(cedula) == True:
                 print(f"El Paciente ya esta en el sistema... Verifique la cedula registrada {cedula}")
                 continue
+            else:
+                p = Paciente()
+                p.asignarCedula(cedula)
+                p.asignarNombre(input("Ingrese el Nombre: "))
+                p.asignarGenero(input("Ingrese el Genero: "))
+                numVis=validar(f"Ingrese el numero de visitas que va ingresar el paciente {p.verNombre()}: ")
 
+                for i in range(numVis):
+                    dia=validar("Ingrese el dia de la visita: ")
+                    mes=validar("Ingrese el mes de la visita: ")
+                    año=validar("Ingrese el año de la visita: ")
+                    f=datetime.date(año,mes,dia)
+
+                    if p.verificarExiste(f):
+                        print("La fecha de la visita ya existe...")
+                        continue
+                    else:
+                        v = Visita()
+                        v.asignarFecha(f)
+                        v.asignarRegistro(os.getcwd()+f'\Pacientes_{p.verCedula()}')
+                        v.asignarNotas(input("Ingrese las observaciones de la visita: "))
+                        ind=v.verIndice()
+                        ind.asignatPot_A1(validarf("Ingrese el indice de a1: "))
+                        ind.asignatPot_A2(validarf("Ingrese el indice de a2: "))
+                        ind.asignatPot_B(validarf("Ingrese el indice de b: "))
+                        ind.asignatPot_G(validarf("Ingrese el indice de g: "))
+                        ind.asignatPot_D(validarf("Ingrese el indice de d: "))
+                        ind.asignatPot_T(validarf("Ingrese el indice de t: "))
+                        v.asignarIndice(ind)  # Reasigno el objeto indice a la visita                   
+
+                        p.asignarVisita(v)
+
+                sis.agregarPaciente(p)
+                print("Paciente agregado con exito...")
+
+
+        elif valor == 2: #Editar paciente y eliminar visita
+            cedula= validar("Ingrese la Cedula: ")
+
+            if sis.verificarExiste(cedula):
+                pac=sis.verPac(cedula)
+                opcion = validar("Ingresar parar editar:\n1- Nombre\n2- Cédula\n3- Género\n4- Visita\n5- Salir\n Opción:  ")
+                if opcion == 1:
+                    pac.asignarNombre(input("Ingrese el nuevo nombre: "))
+                    print("Nombre actualizado con exito...")
+                elif opcion == 2:
+                    pac.asignarCedula(validar("Ingrese la nueva cedula: "))
+                    sis.eliminarPac(cedula)#Elimino el paciente con la cedula anterior
+                    sis.agregarPaciente(pac)#Agrego el paciente con la cedula nueva
+                    print("Cedula actualizada con exito...")
+                elif opcion == 3:
+                    pac.asignarGenero(input("Ingrese el nuevo genero: "))
+                    print("Genero actualizado con exito...")
+                elif opcion == 4:
+                    dia = validar("Ingrese dia:")
+                    mes = validar("Ingrese mes:")
+                    año = validar("Ingrese año:")
+                    f = datetime.datetime(año, mes, dia)
+
+                    if pac.verificarExiste(f)== True:
+                        visi=pac.verVisita(f)
+                        opcion2 = validar("Ingresar parar editar:\n1- Fecha\n2- Registro\n3- Notas\n4- Indice\n5- Eliminar Visita\n Opción:  ")
+                        if opcion2 == 1:
+                            dia = validar("Ingrese dia:")
+                            mes = validar("Ingrese mes:")
+                            año = validar("Ingrese año:")
+                            f = datetime.datetime(año, mes, dia)
+                            visi.asignarFecha(f)#Reasigno la fecha
+                            pac.ingresarVisita(visi)#Reasigno la visita con la fecha nueva
+                            pac.eliminarVisita(f)#Elimino la visita con la fecha anterior
+                            print("Fecha actualizada con exito...")
+                        elif opcion2 == 2:
+                            visi.asignarRegistro(input("Ingrese el nuevo registro: "))
+                            print("Registro actualizado con exito...")
+                        elif opcion2 == 3:
+                            notaExistente = visi.verNotas()
+                            visi.asignarNotas(input("Ingrese nueva Nota complementaria: ")+notaExistente) 
+                            print("Notas actualizadas con exito...")
+                        elif opcion2 == 4:
+                            indi=visi.verIndice()
+                            indi.asignatPot_A1(validarf("Ingrese el indice de a1: "))
+                            indi.asignatPot_A2(validarf("Ingrese el indice de a2: "))
+                            indi.asignatPot_B(validarf("Ingrese el indice de b: "))
+                            indi.asignatPot_G(validarf("Ingrese el indice de g: "))
+                            indi.asignatPot_D(validarf("Ingrese el indice de d: "))
+                            indi.asignatPot_T(validarf("Ingrese el indice de t: "))
+                            visi.asignarIndice(indi)
+                            print("Indice actualizado con exito...")
+                        elif opcion2 == 5:
+                            pac.eliminarVisita(f)# o visi.verFecha()
+                            print("Visita eliminada con exito...")
+            else:
+                print(f"Paciente con cedula {cedula} no existe en el sistema")
+        
+        elif valor == 3:
+            cedula= validar("Ingrese la Cedula: ")
+            if sis.verificarExiste(cedula):
+                sis.eliminarPac(cedula)
+                print("Paciente eliminado con exito...")
+            else:
+                print(f"Paciente con cedula {cedula} no existe en el sistema")
+
+        elif valor == 4: # Cargar y guardar inforamacion a archivo txt
+            cedula= validar("Ingrese la Cedula: ")
+            if sis.verificarExiste(cedula):
+                p = sis.verPac(cedula)
+                archivo = open(f"Paciente {p.verNombre()}.txt",'w')
+                archivo.write(p.verNombre()+ "\n" )
+                archivo.write(str(p.verCedula())+ "\n" )
+                archivo.write(p.verGenero()+ "\n" )
+                for i in p.verListadoVisitas():
+                    archivo.write(p.verVisita(i).verFecha()+ "\n" )
+                    archivo.write(p.verVisita(i).verRegistro()+ "\n" )
+                    archivo.write(p.verVisita(i).verNotas()+ "\n" )
+                    archivo.write(str(p.verVisita(i).verIndice().verPot_A1())+ "\n" )
+                    archivo.write(str(p.verVisita(i).verIndice().verPot_A2())+ "\n" )
+                    archivo.write(str(p.verVisita(i).verIndice().verPot_B()) + "\n" )
+                    archivo.write(str(p.verVisita(i).verIndice().verPot_D()) + "\n" )
+                    archivo.write(str(p.verVisita(i).verIndice().verPot_G()) + "\n" )
+                    archivo.write(str(p.verVisita(i).verIndice().verPot_T()) + "\n") 
+                archivo.close()
+                print("Archivo creado con exito...")
+
+        elif valor == 5: 
+            cedula= validar("Ingrese la Cedula: ")
+            if sis.verificarExiste(cedula):
+                p = sis.verPac(cedula)
+                cv =len(p.verListadoVisitas())
+                print(f"El paciente {p.verNombre()} tiene {cv} visitas")
+
+        elif valor == 6:
+            print("Gracias por usar el sistema...")
+            break
+
+        else:
+            print("Opción no valida, intente de nuevo...")
 if __name__ == "__main__":
     main()
