121bd92d58942bc3a63f346293fcd9d60b83f2e7
EstebanRS1 <esteban.roberto@udea.edu.co>
2023-03-23 08:28:14 -0500

    Matplot

--- matplot.py
+++ matplot.py
@@ -1,6 +1,15 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 20, 100)  
+print(x)  
 plt.plot(x, np.sin(x))       
-plt.show()      
+plt.show()
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# import scipy.io as sio;
+
+# #loading data
+# mat_contents = sio.loadmat('data.mat')#Ubicacion del archivo
